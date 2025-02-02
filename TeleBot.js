bd143834f3c9c56e48f4c7dd69446591584f8328
Weans <137310130+WeansHHN@users.noreply.github.com>
2025-02-02 10:59:50 +0700

    Update TeleBot.js

--- TeleBot.js
+++ TeleBot.js
@@ -1,5 +1,4 @@
-Bot By HaiNinh.site 🤖
-Mua HACK ib @WeansHHN
-
-Hack Free: https://t.me/cheataovfreebyweans/679
-Tham Gia Gr Chém Gió Zalo: https://zalo.me/g/zwzeid009
+{
+  "chatIDs": ["-1002232922242"],
+  "notiMessage": "Bot By HaiNinh.site 🤖\nMua HACK ib @WeansHHN\n\nHack Free: https://t.me/cheataovfreebyweans/679\nTham Gia Gr Chém Gió Zalo: https://zalo.me/g/zwzeid009"
+}
