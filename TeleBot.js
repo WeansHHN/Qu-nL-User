28025821f7475b7e0dfc1f98395538ea4a079c1d
Weans <137310130+WeansHHN@users.noreply.github.com>
2025-02-04 19:42:03 +0700

    Update TeleBot.js

--- TeleBot.js
+++ TeleBot.js
@@ -1,4 +1,3 @@
 {
-  "chatIDs": ["-1002232922242"],
   "notiMessage": "Bot By HaiNinh.site 🤖\nMua HACK ib @WeansHHN\n\nHack Free: https://t.me/cheataovfreebyweans/679\nTham Gia Gr Chém Gió Zalo: https://zalo.me/g/zwzeid009"
 }
